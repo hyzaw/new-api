6e1e576e3b6f5b28a2420846d0fb5d22e97ea91d
hyzaw <2595133804wls@gmail.com>
2026-04-24 01:01:40 +0800

    Change name

--- electron/main.js
+++ electron/main.js
@@ -397,7 +397,7 @@ function createWindow() {
       nodeIntegration: false,
       contextIsolation: true
     },
-    title: 'New API',
+    title: 'Boxying',
     icon: path.join(__dirname, 'icon.png')
   });
 
